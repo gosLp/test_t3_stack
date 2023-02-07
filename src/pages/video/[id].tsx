d769cc23e5dd7c0cce33edbf961a8c960b0b5eeb
Pratheek Shetty <68729469+gosLp@users.noreply.github.com>
2023-02-07 22:18:33 +0530

    video endpoint

--- src/pages/video/[id].tsx
+++ src/pages/video/[id].tsx
@@ -3,16 +3,27 @@ import { useRouter } from "next/router";
 import { string } from "zod";
 import { NextPage } from "next";
 import { ReactNode } from "react";
+import Head from "next/head";
 
 
 const VideoPage: NextPage =() =>{
     const {query} = useRouter();
+
+
     
     const input = query.id?.toString()
     const videoQuery = api.example.hellotest.useQuery({mehtod: input?input: "the id didnt work"});
     const videoListQuery = api.video.list.useQuery();
     return(
+
         <>
+        <Head>
+            <title>Courses</title>
+            <meta name="description" content="The course list" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#FF5F6D] to-[#FFC371]">
         <div>
             <h1>
                 {videoQuery.data?.prat_test}
@@ -26,6 +37,7 @@ const VideoPage: NextPage =() =>{
                 }
             </div>
         </div>
+        </main>
         </>
     );
 
